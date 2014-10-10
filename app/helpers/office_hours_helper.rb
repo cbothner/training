444abfbcfa45a7be7bdcd6b49ccc4e9e23dde52a
キャメロンボスナー <cbothner@umich.edu>
2014-10-10 14:44:51 -0400

    Sort office hours

--- app/helpers/office_hours_helper.rb
+++ app/helpers/office_hours_helper.rb
@@ -1,12 +1,23 @@
 module OfficeHoursHelper
   require 'time'
 
+  # The OFFICE_HOURS constant is used as follows
+  # - `weekday` is a capitalized name of the day of the week a given volunteer's
+  #   office hours are held.
+  # - `start` is the time of day the period starts.  Office hours are assumed to be
+  #   one hour in length.  This value can be any reasonable time format: 1:00PM,
+  #   13h, 1p, 16:30:00, etc.
+  # - `staffer` is the name of the WCBN member who will be in the office.  This
+  #   name is only included in the linewise display of office hours, but is
+  #   included to humanize the situation, and foster a sense of community.
+  # The hours will be displayed in the order they are given below: sort them by day
+  # of the week.
   OFFICE_HOURS = [
-    {weekday: "Thursday", start: "1PM", staffer: "Cameron"},
-    {weekday: "Wednesday", start: "7PM", staffer: "Karl"},
-    {weekday: "Friday", start: "2PM", staffer: "Kristin"},
-    {weekday: "Wednesday", start: "3PM", staffer: "Kirsten"},
     {weekday: "Monday", start: "5PM", staffer: "Max"},
+    {weekday: "Wednesday", start: "3PM", staffer: "Kirsten"},
+    {weekday: "Wednesday", start: "7PM", staffer: "Karl"},
+    {weekday: "Thursday", start: "1PM", staffer: "Cameron"},
+    {weekday: "Friday", start: "2PM", staffer: "Kristin"},
   ]
 
   def linewise_office_hours
