1c2a5ed7cef1659f11ff67aea93f0b5ae7f93d75
キャメロンボスナー <cbothner@umich.edu>
2014-10-10 13:46:24 -0400

    Form Changes: instructions, interest_links helper

--- app/helpers/trainees_helper.rb
+++ app/helpers/trainees_helper.rb
@@ -1,5 +1,10 @@
 module TraineesHelper
   def interest_link(name)
-    '<input type="button" class="interest_link" data-interest="'+name.capitalize+'" value="'+name.capitalize+'" />'
+    '<input type="button" class="interest_link" data-interest="'+name+'" value="'+
+      name+'" />'
+  end
+  
+  def interest_links(list)
+    list.map{ |i| interest_link i }.join.html_safe
   end
 end
